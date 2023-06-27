79faa2442cb339dfb2d3ef0b849ba999f8033b7d
GrigorevaT <76402031+GrigorevaT@users.noreply.github.com>
2023-06-27 15:48:42 +0700

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,15 @@
+valid_email = "student-sf-test@mail.ru"
+valid_password = "Qwe123456!"
+
+invalid_email = "cccq@mai.ru"
+invalid_password = "123456"
+invalid_email_1 = ""
+invalid_number = 79111111111
+invalid_number_1 = 791395
+invalid_number_2 = ""
+invalid_number_3 = 791395415485
+login = "Ivanov"
+personal_account = 100000000000
+
+### параметры для авторизации
+
